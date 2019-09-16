9b61a4081487a435578203917871d8697b684b96
PPS\SRosenthal6608 <FranklinGamedev@gmail.com>
2019-09-16 15:17:05 -0700

    Got entries working properly. Tried to fix generated values

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-import tkinter
+import tkinter as tk
 from tkinter import *
 import math
 from random import randint
 from random import choice
 
-def checkList(row,col):
+class Lotfi(tk.Entry):
+    def __init__(self, master=None, **kwargs):
+        self.var = tk.StringVar()
+        tk.Entry.__init__(self, master, textvariable=self.var, **kwargs)
+        self.old_value = ''
+        self.var.trace('w', self.check)
+        self.get, self.set = self.var.get, self.var.set
+    def check(self, *args):
+        if self.get() is "":
+            # the current value is empty; allow this
+            self.old_value = self.get()
+        elif self.get().isdigit():
+            # the current value is only digits; allow this
+            self.old_value = self.get()
+        else:
+            # there's non-digit characters in the input; reject this
+            self.set(self.old_value)
+        if len(self.get())>1 : self.set(self.get()[:1])
+
+def checkList(row,col): #DOESNT WORK PROPERLY
     output=[]
     for x in range(9):
-        if values[row][col] == None:
-            continue
-        if values[row][col] == values[0][x]:
-            output.append(values[0][x])
-        if values[row][col] == values[x][0]:
-            output.append(values[x][0])
-        if values[row][col] == values[math.floor(row/3)+math.floor(x/3)][math.floor(col/3)+math.floor(x%3)]:
+        if values[row][col] == values[row][x] and values[row][x] != None:
+            output.append(values[row][x])
+        if values[row][col] == values[x][col] and values[x][col] != None:
+            output.append(values[x][row])
+        if values[row][col] == values[math.floor(row/3)+math.floor(x/3)][math.floor(col/3)+math.floor(x%3)] and values[math.floor(row/3)+math.floor(x/3)][math.floor(col/3)+math.floor(x%3)] != None:
             output.append(values[math.floor(row/3)+math.floor(x/3)][math.floor(col/3)+math.floor(x%3)])
     return output
 
@@ -27,27 +44,29 @@ for j,row in enumerate(temp):
         else:
             exist[j][i]=0
 
-temp=None
 values = [[None for i in range(9)] for j in range(9)]
 
 for j in range(9):
     for i in range(9):
         if(exist[j][i]):
-            values[i][j]=choice([i for i in range(0,9) if i not in checkList(j,i)])
+            print(checkList(j,i))
+            temp = choice([k for k in range(0,9) if k not in checkList(j,i)])
+            print(temp)
+            values[j][i]=temp
+            # values[j][i]=choice([i for i in range(0,9) if i not in checkList(j,i)])
 
-root=tkinter.Tk()
+
+root=tk.Tk()
 root.title("Sudoku")
 
 entries = {}
 for j in range(9):
     entries[j]= {}
     for i in range(9):
-        entries[j][i] = Entry(root,width=1)
-        if values[math.floor(i/9)][math.floor(i%9)] is not None:
-            entries[j][i].insert(0,str(values[math.floor(i/9)][math.floor(i%9)]))
+        entries[j][i] = Lotfi(root,width=3)
+        if values[j][i] is not None:
+            entries[j][i].insert(0,str(values[j][i]))
             entries[j][i].configure(state="readonly")
-
-#ALL ENTRIES IN COLUMN ARE THE SAME
 
 for j in range(9):
     for i in range(9):
