ff3452224d29a4fc866ce6c0ab0d67b63ed5fcb6
PPS\SRosenthal6608 <FranklinGamedev@gmail.com>
2019-09-20 09:47:54 -0700

    Fxied exist and entry variables.

--- main.py
+++ main.py
@@ -4,47 +4,66 @@ import math
 from random import randint
 from random import choice
 
-class myApp(tk.Tk):                                             #exist and entries aren't defined
+#status message chaging unfinished
+#win check not finished
+#buttons don't align properly
+
+class myApp(tk.Tk):
     def __init__(self,*args,**kwargs):
         tk.Tk.__init__(self,*args,**kwargs)
-        exist = [[0 for i in range(9)] for j in range(9)]
-        entries = {}
+        self.title("Sudoku")
+        self.entries = {}
+        self.exist = [[0 for i in range(9)] for j in range(9)]
         temp = [[randint(0,9) for i in range(9)] for j in range(9)]
         for j,row in enumerate(temp):
-            entries[j]= {}
+            self.entries[j]= {}
             for i,col in enumerate(row):
                 if(col<4):
-                    exist[j][i]=1
+                    self.exist[j][i]=1
                 else:
-                    exist[j][i]=0
-                entries[j][i] = Lotfi(self,width=3)
-                entries[j][i].configure(font=(20))
-                if(exist[j][i]):
-                    entries[j][i].insert(0,str(choice([k for k in range(9) if k not in self.checkList(j,i)])))
-                    entries[j][i].configure(state="readonly")
-                entries[j][i].grid(row=j,column=i)
-
+                    self.exist[j][i]=0
+                self.entries[j][i] = Lotfi(self,width=3,borderwidth=1, relief="solid")
+                self.entries[j][i].configure(font=(20))
+        for j in range(9):
+            for i in range(9):
+                if(self.exist[j][i]):
+                    self.entries[j][i].insert(0,str(choice([k for k in range(9) if k not in self.checkList(j,i)])))
+                    self.entries[j][i].configure(state="readonly")
+                self.entries[j][i].grid(row=j,column=i)
+        b1 = Button(self,text="Clear",command=self.clear)
+        b2 = Button(self,text="Win check",command=self.win,width=7)
+        l = Label(self,text="Status: Incomplete")
+        b1.grid(row=9,column=5,columnspan=2,sticky=E)
+        b2.grid(row=9,column=7,columnspan=2)
+        l.grid(row=9,column=0,columnspan=4)
     #Returns an array of all values that would be in same "group" as the given coordinate
     def checkList(self,row,col):
         output=[]
         for x in range(9):
-            if entries[row][x].get() != None:
-                output.append(entries[row][x].get())
-            if entries[x][col].get() != None:
-                output.append(entries[x][col].get())
-            if entries[math.floor(row/3)*3+math.floor(x/3)][math.floor(col/3)*3+math.floor(x%3)].get() != None:
-                output.append(entries[math.floor(row/3)*3+math.floor(x/3)][math.floor(col/3)*3+math.floor(x%3)].get())
+            if self.entries[row][x].get() != None:
+                output.append(self.entries[row][x].get())
+            if self.entries[x][col].get() != None:
+                output.append(self.entries[x][col].get())
+            if self.entries[math.floor(row/3)*3+math.floor(x/3)][math.floor(col/3)*3+math.floor(x%3)].get() != None:
+                output.append(self.entries[math.floor(row/3)*3+math.floor(x/3)][math.floor(col/3)*3+math.floor(x%3)].get())
         return output
+
+    #changes status message
+    def setStatus(self):
 
     #Clears all entries
     def clear(self):
         for j in range(9):
             for i in range(9):
-                if exist[j][i] != 1:
-                    entries[j][i].delete(0,END)
+                if self.exist[j][i] != 1:
+                    self.entries[j][i].delete(0,END)
     #Checks if you've won
-    def win():
-        print("test")
+    def win(self):
+        temp=False
+        for j in range(9):
+            for i in range(9):
+                if self.entries[j][i].get() == "":
+                    temp=True
 
 class Lotfi(tk.Entry):
     def __init__(self, master=None, **kwargs):
@@ -66,12 +85,4 @@ class Lotfi(tk.Entry):
         if len(self.get())>1 : self.set(self.get()[:1])
 
 app=myApp()
-app.title("Sudoku")
-
-b1 = Button(root,text="Clear",command=myApp.clear)
-b2 = Button(root,text="Win check",command=myApp.win)
-
-b1.grid(row=9,column=2,columnspan=2,sticky=E)
-b2.grid(row=9,column=5,columnspan=2,sticky=E)
-
 app.mainloop()
